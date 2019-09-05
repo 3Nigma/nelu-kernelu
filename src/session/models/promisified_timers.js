878e446378cd3970a6180318f9332845aecab817
Victor ADASCALITEI <victor.adascalitei@3pillarglobal.com>
2019-09-05 10:15:27 +0300

    Tweaked SessionClearableTimer to support set/clear timer intervals.

--- src/session/models/promisified_timers.js
+++ src/session/models/promisified_timers.js
@@ -1,15 +1,20 @@
 class SessionClearableTimer {
-    constructor(setFun, clrFun, callbackFunc, period, callArgs) {
-        this._timeSetFunc = setFun;
-        this._timeClrFunc = clrFun;
-
+    constructor (
+        timeSetFunc, timeClrFunc, acceptableOnRun, 
+        callbackFunc, period, callArgs) {
+        this._timeClrFunc = timeClrFunc;
+        
         this._timedPromise = new Promise((accept, _) => {
             this._remoteAccept = accept;
             if (period !== undefined) {
                 callArgs.unshift(period);
             }
-            this._internalTimeout = this._timeSetFunc((...args) => {
-                accept(callbackFunc(...args));
+            this._internalTimeout = timeSetFunc((...args) => {
+                const callbackResult = callbackFunc(...args);
+
+                if (acceptableOnRun) {
+                    accept(callbackResult);
+                }
             }, ...callArgs);
         });
     }
@@ -26,19 +31,19 @@ class SessionClearableTimer {
 
 class PromisifiedImediate extends SessionClearableTimer {
     constructor(clb, ...args) {
-        super(setImmediate, clearImmediate, clb, undefined, args);
+        super(setImmediate, clearImmediate, true, clb, undefined, args);
     }
 }
 
 class PromisifiedInterval extends SessionClearableTimer {
     constructor(clb, period, ...args) {
-        super(setInterval, clearInterval, clb, period, args);
+        super(setInterval, clearInterval, false, clb, period, args);
     }
 }
 
 class PromisifiedTimeout extends SessionClearableTimer {
     constructor(clb, period, ...args) {
-        super(setTimeout, clearTimeout, clb, period, args);
+        super(setTimeout, clearTimeout, true, clb, period, args);
     }
 }
 
