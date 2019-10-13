0e46559241401d84e04cfa871fcb2732d0595abf
Stefan-Gabriel Muscalu <stefan.gabriel.muscalu@gmail.com>
2019-10-13 11:55:39 +0300

    fix: kernel.print no longer abbreviates objects

--- src/session/responses/execute_code.js
+++ src/session/responses/execute_code.js
@@ -1,3 +1,4 @@
+const util = require('util');
 const { SessionBasicResponse, SessionBasicResponseTypes } = require('./base');
 
 class SessionExecuteCodeResponse extends SessionBasicResponse {
@@ -21,6 +22,17 @@ class SessionExecuteCodeResponse extends SessionBasicResponse {
                     ename: `${result}`.split(':')[0],
                     evalue: result.message,
                     stack: result.stack
+                };
+            } else if (typeof result === 'object') {
+                this._result = {
+                    type: 'ok',
+                    result: util.inspect(result, {
+                        depth: 10,
+                        maxArrayLength: 100,
+                        breakLength: 120,
+                        showHidden: true,
+                        showProxy: true
+                    })
                 };
             } else {
                 this._result = {
